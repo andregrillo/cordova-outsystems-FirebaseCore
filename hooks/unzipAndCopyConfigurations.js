0569d5d040e3b343f9e8c3d60a7465757b77a720
André Grillo <andregrillo@gmail.com>
2021-05-28 12:43:59 +0100

    Revert "Troubleshooting copy hook"
    This reverts commit b4c5c42421dbd911406711c0e6d7d0a82f3b8565.

--- hooks/unzipAndCopyConfigurations.js
+++ hooks/unzipAndCopyConfigurations.js
@@ -50,8 +50,6 @@ module.exports = function(context) {
   }
 
   var sourceFilePath = path.join(targetPath, fileName);
-  console.log("🚨 targetPath: " + targetPath);
-  console.log("🚨 Filepath: " + fileName);
   var destFilePath = path.join(context.opts.plugin.dir, fileName);
 
   if(!utils.checkIfFolderExists(destFilePath)){
